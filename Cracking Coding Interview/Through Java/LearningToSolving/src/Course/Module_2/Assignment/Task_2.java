ca83ed1c7beac63f768135a2a5c4d74ea35eed59
shuvomonowar <monowarhossain41998@gmail.com>
2023-12-11 23:48:39 +0600

    Module 2 assignment is updated using java programming language

--- Cracking Coding Interview/Through Java/LearningToSolving/src/Course/Module_2/Assignment/Task_2.java
+++ Cracking Coding Interview/Through Java/LearningToSolving/src/Course/Module_2/Assignment/Task_2.java
@@ -3,19 +3,49 @@ package Course.Module_2.Assignment;
 import java.util.Scanner;
 
 public class Task_2 {
-    void solution() {
-        Scanner sc = new Scanner(System.in);
+    public int solution(int[] prices) {
+        int l = prices.length, tp = 0, mp = 0, j = 1;
+        for (int i = 0; i < l-1; i++) {
+            if (j >= l) {
+                break;
+            }
+            else if (prices[i] < prices[j]) {
+                tp = prices[j]-prices[i];
+                if (tp > mp) {
+                    mp = tp;
+                }
+                j++;
+                i--;
+            } else {
+                tp = prices[j]-prices[i];
+                if (tp > mp) {
+                    mp = tp;
+                }
+                i = j-1;
+                j++;
+            }
+        }
 
-
+        return mp;
     }
 
     // Main method
     public static void main(String[] args) {
-        Task_2 task2 = new Task_2();
-        task2.solution();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+        System.out.print("Enter the elements of the array: ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Task_2 obj1 = new Task_2();
+        System.out.println(obj1.solution(arr));
     }
 }
 
 
-// Time Complexity : O()
-// Space Complexity : O()
+// Time Complexity : O(N)
+// Space Complexity : O(N)
