73b47a089f1571bcaa7472036f1b2f12e711bb68
shuvomonowar <monowarhossain41998@gmail.com>
2023-12-18 23:21:11 +0600

    Module 3 assignment is initially updated using Java programming language

---
+++ Cracking Coding Interview/Through Java/LearningToSolving/src/Course/Module_3/Assignment/Task_1.java
@@ -0,0 +1,18 @@
+package Course.Module_3.Assignment;
+
+public class Task_1 {
+    // Solution of the task 1
+    void solution() {
+        
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        Task_1 task1 = new Task_1();
+        task1.solution();
+    }
+}
+
+
+// Time Complexity : O()
+// Space Complexity : O()
---
+++ Cracking Coding Interview/Through Java/LearningToSolving/src/Course/Module_3/Assignment/Task_2.java
@@ -0,0 +1,18 @@
+package Course.Module_3.Assignment;
+
+public class Task_2 {
+    // Solution of the task 2
+    public int solution() {
+		
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        Task_2 obj1 = new Task_2();
+        System.out.println(obj1.solution());
+    }
+}
+
+
+// Time Complexity : O()
+// Space Complexity : O()
