4d543e5cff8970317cf5037ae994bacd1af0ae8f
shuvomonowar <monowarhossain41998@gmail.com>
2023-12-19 00:49:23 +0600

    Module 3 assignment task 1 is fully completed using java programming language

--- Cracking Coding Interview/Through Java/LearningToSolving/src/Course/Module_3/Assignment/Task_1.java
+++ Cracking Coding Interview/Through Java/LearningToSolving/src/Course/Module_3/Assignment/Task_1.java
@@ -1,18 +1,36 @@
 package Course.Module_3.Assignment;
 
+import java.util.Scanner;
+
 public class Task_1 {
     // Solution of the task 1
-    void solution() {
-        
+    void solution(String s, int[] indices) {
+        String fs = "";
+        for (int i = 0; i < indices.length; i++) {
+            fs  = fs.concat(String.valueOf(s.charAt(indices[i])));
+        }
+
+        System.out.println(fs);
     }
 
     // Main method
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String s = sc.nextLine();
+
+        int n = s.length();
+        int[] indices = new int[n];
+        System.out.print("Enter the indices: ");
+        for (int i = 0; i < n; i++) {
+            indices[i] = sc.nextInt();
+        }
+
         Task_1 task1 = new Task_1();
-        task1.solution();
+        task1.solution(s, indices);
     }
 }
 
 
-// Time Complexity : O()
-// Space Complexity : O()
+// Time Complexity : O(N)
+// Space Complexity : O(N)
--- Cracking Coding Interview/Through Java/LearningToSolving/src/Course/Module_3/Assignment/Task_2.java
+++ Cracking Coding Interview/Through Java/LearningToSolving/src/Course/Module_3/Assignment/Task_2.java
@@ -1,15 +1,25 @@
 package Course.Module_3.Assignment;
 
+import java.util.Scanner;
+
 public class Task_2 {
     // Solution of the task 2
-    public int solution() {
-		
+    public void solution(String h, String n) {
+        for (int i = 0; i < h.length(); i++) {
+            
+        }
     }
 
     // Main method
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the haystack string: ");
+        String h = sc.nextLine();
+        System.out.print("Enter the needle string: ");
+        String n = sc.nextLine();
+
         Task_2 obj1 = new Task_2();
-        System.out.println(obj1.solution());
+        obj1.solution(h, n);
     }
 }
 
