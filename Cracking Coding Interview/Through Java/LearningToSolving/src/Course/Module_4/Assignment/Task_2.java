f26c2e692b1a5bb3d3edc6e615e224367c77f07e
shuvomonowar <monowarhossain41998@gmail.com>
2023-12-28 00:32:21 +0600

    Module 4 assignment task 2 is fully completed using java programming language

--- Cracking Coding Interview/Through Java/LearningToSolving/src/Course/Module_4/Assignment/Task_2.java
+++ Cracking Coding Interview/Through Java/LearningToSolving/src/Course/Module_4/Assignment/Task_2.java
@@ -1,18 +1,30 @@
 package Course.Module_4.Assignment;
 
+import java.util.Scanner;
+
 public class Task_2 {
     // Solution of the task 2
-    public void solution() {
-
+    int solution(int a, int b) {
+        if (b < 1) {
+            return 0;
+        }
+        return a+solution(a, b-1);
     }
 
     // Main method
     public static void main(String[] args) {
+        // User input
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int b = sc.nextInt();
+
         Task_2 obj1 = new Task_2();
-        obj1.solution();
+        System.out.println("The result of multiplication: " + obj1.solution(a, b));
     }
 }
 
 
-// Time Complexity : O()
-// Space Complexity : O()
+// Time Complexity : O(n)
+// Space Complexity : O(n)
