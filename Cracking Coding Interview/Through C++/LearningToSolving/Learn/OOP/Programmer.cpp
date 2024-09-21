cebe2a7c7c64d8131cafc19c48ce7b07dee1f9d5
shuvomonowar <monowarhossain41998@gmail.com>
2024-09-21 21:12:37 +0600

    In C++ section in learning phase in OOP section a new HelloProgrammer class is created

---
+++ Cracking Coding Interview/Through C++/LearningToSolving/Learn/OOP/Programmer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Programmer {
+    public:
+        Programmer() {
+            cout << "Hello I'm a programmer :)\n";
+        }
+};
+
+int main() {
+    Programmer prgrmr;
+
+    return 0;
+}
